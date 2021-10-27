dfae1f35fe0eccd4fc5aaf41e5127376fbc3b532
Chase's Hub <chaseghorton99@gmail.com>
2021-10-27 15:55:19 -0500

    Coded InputController

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public event Action PressedConfirm = delegate { };
+    public event Action PressedCancel = delegate { };
+    public event Action PressedLeft = delegate { };
+    public event Action PressedRight = delegate { };
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DetectConfirm();
+        DetectCancel();
+        DetectLeft();
+        DetectRight();
     }
+
+    private void DetectRight()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            PressedRight?.Invoke();
+        }
+    }
+
+    private void DetectLeft()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PressedLeft?.Invoke();
+        }
+    }
+
+    private void DetectCancel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PressedCancel?.Invoke();
+        }
+    }
+
+    private void DetectConfirm()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PressedConfirm?.Invoke();
+        }
+    }
+
 }
