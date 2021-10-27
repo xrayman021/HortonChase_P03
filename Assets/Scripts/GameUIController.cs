47bb91e2a141688f6f81f429d85a617feed98249
Chase's Hub <chaseghorton99@gmail.com>
2021-10-27 16:18:26 -0500

    Created and coded UIController

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    [SerializeField] Text _enemyThinkingTextUI = null;
+
+    private void OnEnable()
+    {
+        EnemyTurnGameState.EnemyTurnBegan += OnEnemyTurnBegan;
+        EnemyTurnGameState.EnemyTurnEnded += OnEnemyTurnEnded;
+    }
+
+    private void OnDisable()
+    {
+        EnemyTurnGameState.EnemyTurnBegan -= OnEnemyTurnBegan;
+        EnemyTurnGameState.EnemyTurnEnded -= OnEnemyTurnEnded;
+    }
+
+    private void Start()
+    {
+        _enemyThinkingTextUI.gameObject.SetActive(false);
+    }
+
+    void OnEnemyTurnBegan()
+    {
+        _enemyThinkingTextUI.gameObject.SetActive(true);
+    }
+
+    void OnEnemyTurnEnded()
+    {
+        _enemyThinkingTextUI.gameObject.SetActive(false);
+    }
+}
