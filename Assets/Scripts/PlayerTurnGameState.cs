0a0486531f34d0a6d40a77c1ac89612d13a3d057
Chase's Hub <chaseghorton99@gmail.com>
2021-10-27 15:16:09 -0500

    Created and coded PlayerTurnGameState

---
+++ Assets/Scripts/PlayerTurnGameState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTurnGameState : GameState
+{
+    [SerializeField] Text _playerTurnTextUI = null;
+
+    int _playerTurnCount = 0;
+
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.Log("Player turn: ...Entering");
+        _playerTurnTextUI.gameObject.SetActive(true);
+
+        _playerTurnCount++;
+        _playerTurnTextUI.text = "Player Turn: " + _playerTurnCount.ToString();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        _playerTurnTextUI.gameObject.SetActive(false);
+        Debug.Log("Player Turn: Exiting...");
+    }
+
+}
