45d62f07e59dfbbcb871f27dcfb2fb7d1be332a1
Chase's Hub <chaseghorton99@gmail.com>
2021-10-27 14:19:02 -0500

    Coded GameSM

--- Assets/Scripts/GameSM.cs
+++ Assets/Scripts/GameSM.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameSM : MonoBehaviour
+public class GameSM : StateMachine
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //set starting State here
     }
 }
