824a73442a31bab68049574b73c7b7e507ba2779
Chase's Hub <chaseghorton99@gmail.com>
2021-10-27 14:17:06 -0500

    Coded StateMachine

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour
+public abstract class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public State CurrentState => _currentState;
+    protected bool InTransition { get; private set; }
+
+    State _currentState;
+    protected State _previousState;
+
+    public void ChangeState<T>() where T : State
     {
-        
+        T targetState = GetComponent<T>();
+        if(targetState == null)
+        {
+            Debug.LogWarning("Cannot change to state, as it " +
+                "does not exist on the State Machine Object." +
+                " Make sure you have the desired State attached " +
+                "to the State Machine!");
+            return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RevertState()
     {
-        
+        if(_previousState != null)
+        {
+            InitiateStateChange(_previousState);
+        }
     }
+
+    void InitiateStateChange(State targetState)
+    {
+        if(_currentState != targetState && !InTransition)
+        {
+            Transition(targetState);
+        }
+    }
+
+    void Transition(State newState)
+    {
+        InTransition = true;
+        _currentState?.Exit();
+        _currentState = newState;
+        _currentState?.Enter();
+        InTransition = false;
+    }
+
+    private void Update()
+    {
+        if(CurrentState != null && !InTransition)
+        {
+            CurrentState.Tick();
+        }
+    }
+
 }
