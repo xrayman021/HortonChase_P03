7c6d0c860aa5af84b929e7c9cf90faa45a72a3cd
Chase's Hub <chaseghorton99@gmail.com>
2021-11-01 14:40:32 -0500

    Created and coded scripts for playing audio, imported two songs

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    void Start()
+    {
+        if (_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+}
