1eb28b605588b17adb12e0f1e42cbc38fe070436
Chase's Hub <chaseghorton99@gmail.com>
2021-11-12 15:10:06 -0600

    Further coded SpawnToken

--- Assets/Scripts/SpawnTokenCommand.cs
+++ Assets/Scripts/SpawnTokenCommand.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SpawnTokenCommand : ICommand
 {
-    public SpawnTokenCommand()
-    {
+    BoardSpawner _boardSpawner;
+    Vector3 _position;
 
+    Token _spawnedToken;
+
+    public SpawnTokenCommand(BoardSpawner boardSpawner, Vector3 position)
+    {
+        _boardSpawner = boardSpawner;
+        _position = position;
     }
 
     public void Execute()
     {
+        _spawnedToken = _boardSpawner.SpawnToken(_position);
         Debug.Log("Spawn Token");
     }
 }
