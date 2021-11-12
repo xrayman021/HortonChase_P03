e00fd7514713fc88b4a0392a29e12ab344e67f32
Chase's Hub <chaseghorton99@gmail.com>
2021-11-12 15:01:31 -0600

    Created and coded BoardSpawner script

---
+++ Assets/Scripts/BoardSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSpawner : MonoBehaviour
+{
+    [SerializeField] Token _tokenPrefab = null;
+
+    public Token SpawnToken(Vector3 position)
+    {
+        Token newToken = Instantiate(_tokenPrefab,
+            position, _tokenPrefab.transform.rotation);
+
+        return newToken;
+    }
+}
