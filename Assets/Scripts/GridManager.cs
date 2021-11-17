a2dbbb7112983d361ae3d35e27130d7ff639f6b8
Chase's Hub <chaseghorton99@gmail.com>
2021-11-17 14:55:41 -0600

    Created GridManager and Tile scripts, created Tile prefab

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+    [SerializeField] private Tile _tilePrefab;
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+
+            }
+        }
+    }
+}
