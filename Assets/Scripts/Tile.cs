0f2d6e3224e078d58045861f744428f09552da0f
Chase's Hub <chaseghorton99@gmail.com>
2021-11-17 15:45:00 -0600

    Added HiLight code to Tile

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _highlight;
+
+    private void OnMouseEnter()
     {
-        
+        _highlight.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        _highlight.SetActive(false);
     }
 }
