1dbf7552201c6e3435f3046a201b049325996250
Chase's Hub <chaseghorton99@gmail.com>
2021-11-17 17:55:39 -0600

    Coded new GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,32 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public GameState GameState;
+
+    public static event Action<GameState> OnGameStateChanged;
+
     void Awake()
     {
         Instance = this;
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        ChangeState(GameState.GenerateGrid);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeState(GameState newState)
     {
-        
+        GameState = newState;
+        switch (newState)
+        {
+            case GameState.GenerateGrid:
+                HandlePlayerTurn();
+                break;
+            case GameState.SpawnHeroes:
+                break;
+            case GameState.SpawnEnemies:
+                break;
+            case GameState.HeroesTurn:
+                break;
+            case GameState.EnemiesTurn:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        OnGameStateChanged?.Invoke(newState);
     }
+
+    private void HandlePlayerTurn()
+    {
+
+    }
+
 }
 
 public enum GameState
 {
-    PlayerTurn,
-    EnemyTurn,
-    Victory,
-    Lose
+    GenerateGrid = 0,
+    SpawnHeroes = 1,
+    SpawnEnemies = 2,
+    HeroesTurn = 3,
+    EnemiesTurn = 4
+
 }
--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class UnitManager : MonoBehaviour
 {
     public static UnitManager Instance;
 
+    //[SerializeField] private List<Unit> _units;
+
     void Awake()
     {
         Instance = this;
+    }
+
+    public async void Attack(Faction faction)
+    {
+
     }
 }
